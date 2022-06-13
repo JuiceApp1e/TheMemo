929cbf6a246d3b046c1700a5d73c137eadc1915c
ZiguoCheng <2746738323@qq.com>
2022-06-13 15:39:29 +0800

    "Get openid"

--- controller/todocontroller.go
+++ controller/todocontroller.go
@@ -43,9 +43,10 @@ func GetOpenId(c *gin.Context) {
 	)
 	//获取code
 	code := c.Query("code")
-
+	fmt.Println(code)
 	//调用auth.code2Session接口获取openid
 	url := fmt.Sprintf(code2sessionURL, appID, appSecret, code)
+	fmt.Println(url)
 	resp, err := http.Get(url)
 	if err != nil {
 		return
